bcbb03acc9243bd5c9e4a2e765f4ace81542e498
Hung Manh Tran <hungtran@kms-technology.com>
2018-12-05 09:59:47 +0700

    Web UI Basic

---
+++ Test Suites/Delivery/Release 1-Sprint 1/Collection_1.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>Collection_1</name>
+   <tag></tag>
+   <executionMode>SEQUENTIAL</executionMode>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>github</profileName>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Delivery/Release 1-Sprint 1/Suite_1</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
